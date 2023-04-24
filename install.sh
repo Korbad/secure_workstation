c68e7265ea7f9a8c0dfee214cfc3361f65d4969c
korbad <korbad@gmail.com>
2023-04-24 13:14:40 -0500

    Adds installation of korbad.project_management collection to install.sh

--- install.sh
+++ install.sh
@@ -8,3 +8,12 @@ sudo pacman -S ansible --noconfirm
 
 # Verify installation
 ansible --version
+
+# Install the korbad.project_management collection
+ansible-pull -U git@github.com:Korbad/ansible-collection-korbad.project_management.git -i localhost, --accept-host-key --clean --purge
+
+# # Download the secure_workstation playbooks (using the project_management collection)
+# ansible-pull -U git@github.com:Korbad/secure_workstation.git -i localhost, --accept-host-key --clean --purge
+
+# # Set up workstation configuration
+# ansible-playbook ~/managed_git_repos/secure_workstation/workstation.yml
