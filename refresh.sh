2e9754cc1404b2824a4d0a2421c2cf5db07b7243
korbad <korbad@gmail.com>
2023-04-26 19:18:26 -0500

    Adds playbook which performs prerequisites for workstation playbook

--- refresh.sh
+++ refresh.sh
@@ -4,4 +4,7 @@
 ansible-playbook ~/managed_git_repos/korbad_secure_workstation/refresh_managed_artifacts.yml
 
 # Refresh current workstation setup
+ansible-playbook ~/managed_git_repos/korbad_secure_workstation/workstation_base.yml
+
+# Refresh current workstation setup
 ansible-playbook ~/managed_git_repos/korbad_secure_workstation/refresh.yml
