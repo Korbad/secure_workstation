4be59448e64e72bd3c1da2d2a546d5284ae04464
korbad <korbad@gmail.com>
2023-04-30 11:22:04 -0500

    Adds playbook which performs host-specific tasks

--- refresh.sh
+++ refresh.sh
@@ -6,6 +6,8 @@ ansible-playbook ~/managed_git_repos/korbad_secure_workstation/refresh_managed_a
 # Refresh current workstation setup
 ansible-playbook ~/managed_git_repos/korbad_secure_workstation/pre_sudoers.yml
 
-# Perform installations that require sudo
-ansible-playbook ~/managed_git_repos/korbad_secure_workstation/post_sudoers.yml
+# Perform installations after sudo perms are set and vault file exists
+ansible-playbook ~/managed_git_repos/korbad_secure_workstation/post_sudoers.yml --vault-password-file=~/managed_vault_files/vault_password_file
 
+# Execute host-specific playbook
+ansible-playbook ~/managed_git_repos/korbad_secure_workstation/custom.yml --vault-password-file=~/managed_vault_files/vault_password_file
