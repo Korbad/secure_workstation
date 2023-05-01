cdf6a17d0af3ccfc679fc92f157bce03aa9bb982
korbad <korbad@gmail.com>
2023-05-01 12:42:13 -0500

    Moves collection and secure workstation out of install.sh and install.sh needs root perms

--- install_collections.sh
+++ install_collections.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Install the korbad.project_management collection
+ansible-pull -U https://github.com/Korbad/ansible-collection-korbad.project_management.git -i localhost, --accept-host-key --clean --purge
+
+# Download the secure_workstation playbooks (using the project_management collection)
+ansible-pull -U https://github.com/Korbad/secure_workstation.git -i localhost, --accept-host-key --clean --purge
+
+
 ansible-playbook ~/managed_git_repos/korbad_secure_workstation/install_collections.yml
