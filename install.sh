203cb3464c009824e61d813092bc13feb9c2a5cd
Joshua Perry <Korbad@gmail.com>
2023-05-03 11:47:08 -0500

    Adds ansible pull commands to get the project management collection and this repo itself

--- install.sh
+++ install.sh
@@ -8,3 +8,9 @@ sudo pacman -S ansible --noconfirm
 
 # Verify installation
 ansible --version
+
+# Install the korbad.project_management collection
+ansible-pull -U https://github.com/Korbad/ansible-collection-korbad.project_management.git -i localhost, --accept-host-key --clean --purge
+
+# Download the secure_workstation playbooks (using the project_management collection)
+ansible-pull -U https://github.com/Korbad/secure_workstation.git -i localhost, --accept-host-key --clean --purge
