deb911ceb83e9bc41551231d84803b03f7e4793b
Joshua Perry <Korbad@gmail.com>
2023-05-03 14:44:42 -0500

    Adds command line ask-become-pass to pre-sudoers playbook

--- pre_sudoers.sh
+++ pre_sudoers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # # Refresh current workstation setup
-ansible-playbook ~/managed_git_repos/korbad_secure_workstation/pre_sudoers.yml
+ansible-playbook ~/managed_git_repos/korbad_secure_workstation/pre_sudoers.yml --ask-become-pass
