6bb0464d5627f67d808bb085cf295b18d5e353da
korbad <korbad@gmail.com>
2023-05-04 15:11:10 -0500

    Moves "vault password file management" from "pre_sudoers" to "post_sudoers"

--- post_sudoers.sh
+++ post_sudoers.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ansible-playbook ~/managed_git_repos/korbad_secure_workstation/vault_password_file.yml
 
 # # Perform installations after sudo perms are set and vault file exists
 ansible-playbook ~/managed_git_repos/korbad_secure_workstation/post_sudoers.yml --vault-password-file=~/managed_vault_files/vault_password_file
