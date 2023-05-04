773dc5029f93175f394ab516d894b1618f649030
Joshua Perry <Korbad@gmail.com>
2023-05-03 20:18:16 -0500

    Updates refresh script to use refresh playbook

--- refresh.sh
+++ refresh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # # Refresh current managed content
-ansible-playbook ~/managed_git_repos/korbad_secure_workstation/install_collections.yml
+ansible-playbook ~/managed_git_repos/korbad_secure_workstation/refresh.yml
 
 # # Execute host-specific playbook
 # ansible-playbook ~/managed_git_repos/korbad_secure_workstation/custom.yml --vault-password-file=~/managed_vault_files/vault_password_file -i inventory.yml
