5cde0dc1d34251283b9924e617a17ba1af712b7a
korbad <korbad@gmail.com>
2023-05-13 01:59:42 -0500

    Updates refresh script to better perform a refresh of managed conent

--- refresh.sh
+++ refresh.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+ansible-playbook ~/managed_git_repos/korbad_secure_workstation/refresh_project_manager.yml
 
-# # Refresh current managed content
-ansible-playbook ~/managed_git_repos/korbad_secure_workstation/refresh.yml
+ansible-playbook ~/managed_git_repos/korbad_secure_workstation/refresh_korbad.yml
 
-# # Execute host-specific playbook
-# ansible-playbook ~/managed_git_repos/korbad_secure_workstation/custom.yml --vault-password-file=~/managed_vault_files/vault_password_file -i inventory.yml
+# ansible-playbook ~/managed_git_repos/korbad_secure_workstation/refresh_repos.yml
