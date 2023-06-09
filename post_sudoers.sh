69bfaa9ec191aa449a50a265bc1ca8797ee837dc
korbad <korbad@gmail.com>
2023-06-09 16:04:44 -0500

    Updates vault password file default location

--- post_sudoers.sh
+++ post_sudoers.sh
@@ -2,4 +2,4 @@
 ansible-playbook ~/managed_git_repos/korbad_secure_workstation/vault_password_file.yml
 
 # # Perform installations after sudo perms are set and vault file exists
-ansible-playbook ~/managed_git_repos/korbad_secure_workstation/post_sudoers.yml --vault-password-file=~/managed_vault_files/vault_password_file
+ansible-playbook ~/managed_git_repos/korbad_secure_workstation/post_sudoers.yml --vault-password-file=~/managed_secrets/default_vault/default_vault
